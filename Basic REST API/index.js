096ad04b6c41b251eeff5d9c758e5cd9446c460e
Krantesh Singh <34528823+KranteshSingh@users.noreply.github.com>
2019-08-25 19:46:16 +0530

    Fixed Bug

--- Basic REST API/index.js
+++ Basic REST API/index.js
@@ -1,11 +1,19 @@
-// this is needed for importing expressjs into our application
+// This is needed for importing expressjs into our application
 const express = require('express')
 const appConfig = require('./config/appConfig')
-// declaring an instance or creating an application instance
+const fs = require('fs')
+const port = 3000
+
+// Declaring an instance or creating an application instance
 const app = express()
 
-let helloWorldFunction = (req, res) => res.send('Hello World!')
+let routesPath = './routes'
+fs.readdirSync(routesPath).forEach(function (file) {
+    if (~file.indexOf('.js')){
+        let route = require(routesPath + '/' + file);
+        route.setRouter(app);
+    }
+});
 
-app.get('/', helloWorldFunction)
-
-app.listen(appConfig.port, () => console.log(`Example app listening on port 3000!`))
+//Listening the server - creating a local server
+app.listen(port, () => console.log('Example app listening on port 3000!'))
--- Basic REST API/routes/blog.js
+++ Basic REST API/routes/blog.js
@@ -1,11 +1,15 @@
-const express = require("express");
-const blogController = require("./../controllers/blogController");
+const express = require('express')
+// Including the logic in controller & including that module here
+const blogController = require('./../controllers/blogController');
 
-let setRouter = app => {
-  app.get("/hello-world", blogController.helloWorld);
-  app.get("/example", blogController.printExample);
+let setRouter = (app) => {
+  
+  app.get('/hello-world', blogController.helloWorld);
+  app.get('/example', blogController.printExample);
+
+
 }; // end setRouter function
 
 module.exports = {
   setRouter: setRouter
-};
+}
