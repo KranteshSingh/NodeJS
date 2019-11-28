8d97f715894845daf8e21d8a1597b1919e1d0e9a
Krantesh Singh <34528823+KranteshSingh@users.noreply.github.com>
2019-11-28 09:36:04 +0530

    Express

--- CWM/04 Advanced Topics/01 Middleware/index.js
+++ CWM/04 Advanced Topics/01 Middleware/index.js
@@ -1,10 +1,18 @@
 const config = require('config');
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
 const Joi = require('joi');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const logger = require('./logger');
+const courses = require('./router/courses');
+const home = require('./routes/courses');
+const logger = require('./middleware/logger');
 const express = require('express');
 const app = express();
+
+// Templating engine for send HTML markup for http requests
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 process.env.NODE_ENV;
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
@@ -14,7 +22,11 @@ console.log(`app: ${app.get('env')}`);
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
   console.log('Morgan enabled...');
+  startupDebugger('Morgan enabled...');
 }
+
+// DB WORK
+dbDebugger('Connected to the database');
 
 // Built in middlewares
 app.use(express.json());
@@ -23,18 +35,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(helmet());
 app.use(morgan('tiny'));
-
-const courses = [
-  { id: 1, name: 'Course 1' },
-  { id: 2, name: 'Course 2' },
-  { id: 3, name: 'Course 3' },
-  { id: 4, name: 'Course 4' }
-];
+app.use('/api/courses', courses);
+app.use('/home', home);
 
 // Configurations
 
 console.log('Application Name:' + config.get('name'));
 console.log('Mail Server:' + config.get('mail.host'));
+console.log('Mail Password:' + config.get('mail.password'));
 
 // Concept of middleware
 app.use(logger);
@@ -46,76 +54,8 @@ app.use(function(req, res, next) {
 
 // Testing for the node application if its works or not
 app.get('/', (req, res) => {
-  res.send('HELLO WORLD');
-});
-
-// Getting all the courses we have
-app.get('/api/courses', (req, res) => {
-  res.send(courses);
-});
-
-// Creating a new course through POST request
-app.post('/api/courses', (req, res) => {
-  const { error } = validateCourse(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name
-  };
-  courses.push(course);
-  res.send(course);
-});
-
-// Updating a course name within the list of courses
-app.put('/api/courses/:id', (req, res) => {
-  // Look up the course
-  // If not existing, return 404
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course)
-    return res.status(404).send('The course with the given ID was not found');
-
-  // Validate
-  // If invalid, return 400 - Bad request
-  const { error } = validateCourse(req.body); // result.error
-  if (error) return res.status(400).send(error.details[0].message);
-
-  // Update course
-  // Return the updated course
-  course.name = req.body.name;
-  res.send(course);
-});
-
-// Reusable function for Validation
-function validateCourse(course) {
-  const schema = {
-    name: Joi.string()
-      .min(3)
-      .required()
-  };
-
-  return Joi.validate(course, schema);
-}
-
-// Getting detail of one course through route parameter
-app.get('/api/courses/:id', (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course)
-    return res.status(404).send('the course with the given id was not found');
-  res.send(course);
-});
-
-// Deleting any single course through its route parameter.
-app.delete('/api/courses/:id', (req, res) => {
-  // Look up the course
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course)
-    return res.status(404).send('The course with the given ID was not found');
-
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-
-  res.send(course);
+  //res.send('HELLO WORLD');
+  res.render('index', { title: 'My Express app', message: 'Hello You' });
 });
 
 // Defining the configuration for Application
